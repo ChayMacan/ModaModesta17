0aab82fb98be06c389fe2713eeb19a1d69fff7b6
Radio escola <133807868+ChayMacan@users.noreply.github.com>
2024-05-14 10:55:09 -0300

    ghfhf

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-onst botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
@@ -34,23 +34,25 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    if (tempoFinal >0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
     } else {
-        return "prazo finalizado";
+        return [0,0,0,0];
+        }
     }
-      
-}
 
-function atualizacronometro(){
-    for(let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculatempo(tempos[i]);
+    function atualizaCronometro(){
+        for(let i=0; i<contadores.length;i++){
+document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
      }
 }
 
-function comecacronometro(){
-    atualizacronometro();
-setInterval(atualizacronometro,1000);
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
 }
 
-comecacronometro();
+comecaCronometro();
